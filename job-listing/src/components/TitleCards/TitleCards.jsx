3b136897c64f259177408911a71083787d0932c1
Auwalu Muhammad Izziddin <117743968+Auwaluncy@users.noreply.github.com>
2024-11-07 12:24:34 +0100

    second commit
    created the titlecard and player components

--- job-listing/src/components/TitleCards/TitleCards.jsx
+++ job-listing/src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,24 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
-
+import { Link } from 'react-router-dom'
  
 
 const TitleCards = ({title, category}) => {
 
+  const [apiData, setApiData] = useState([])
+
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjc5MTEzMTBlMjEyODBkNTdmNjJiNTQ2YmVlODBkNSIsIm5iZiI6MTczMDkyNzgzNC41NjI1NDY3LCJzdWIiOiI2NzJiZGEzMGRlNTQ2NTg1ZDA0ZGE4YzEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.oqYz4F-PNFHdtQ6zOlyreoQR-GinuXPXIgPYApoFsw8'
+    }
+  };
+  
+  
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -14,18 +26,29 @@ const TitleCards = ({title, category}) => {
   }
   
   useEffect(()=>{
+
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
+
+
     cardsRef.current.addEventListener("wheel", handleWheel)
   },[])
+
+
+
 
   return (
     <div className='titlecards'>
       <h2>{title ? title : "Popular on NetFlix" }</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
-          return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-          </div>
+        {apiData.map((card, index) => {
+          return <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
+          </Link>
         })}
       </div>
     </div>
--- job-listing/src/pages/Home/Home.jsx
+++ job-listing/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info from '../../assets/info_icon.png'
 import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
 
 
 const Home = () => {
@@ -23,6 +24,7 @@ const Home = () => {
                     </p>
                     <div className="hero-btns">
                         <button className='btn'><img src={play_icon} alt="" />Play</button>
+                  
                         <button className='btn dark-btn'><img src={info} alt="" />More Info</button>
                     </div>
 
@@ -31,11 +33,14 @@ const Home = () => {
                 </div>
             </div>
             <div className="more-cards">
-                 <TitleCards title={"Blockbuster Movies"} />
-                 <TitleCards title={"Only on Netflix"} />
-                 <TitleCards title={"Upcoming"} />
-                 <TitleCards title={"Top Pics for you"} />
+                 <TitleCards category={"top_rated"} title={"Blockbuster Movies"} />
+                 <TitleCards category={"popular"} title={"Only on Netflix"} />
+                 <TitleCards category={"upcoming"} title={"Upcoming"} />
+                 <TitleCards category={"now_playing"} title={"Top Pics for you"} />
             </div>
+
+             {/* Footer components */}
+            <Footer />
         </div>
     )
 }
